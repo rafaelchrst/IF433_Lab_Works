02063729112aa77f9eff711a1232908f97aa03d1
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-02-12 23:31:41 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000135764_RafaelChristhiano/week01/GameStore.kt
+++ src/oop_00000135764_RafaelChristhiano/week01/GameStore.kt
@@ -4,4 +4,8 @@ fun main(){
     val gameTitle = "GTA 7"
     val price = 600000
 
+    var finalPrice = calculateDiscount(price)
+    println(finalPrice)
 }
+
+fun calculateDiscount(price:Int): Int = if (price > 500000) price - (price * 20/100) else price - (price * 10/100)

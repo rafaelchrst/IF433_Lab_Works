4b65c9b258513bc6ae83447936fc9ac2db44bc79
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-02-12 23:23:48 +0700

    week01: (tugas 1) add legacy CircleCalculator codeweek01: (tugas 1) refactor variables to val and use string

--- src/oop_00000135764_RafaelChristhiano/week01/CircleCalculator.kt
+++ src/oop_00000135764_RafaelChristhiano/week01/CircleCalculator.kt
@@ -2,12 +2,12 @@ package oop_00000135764_RafaelChristhiano.week01
 
 fun main(args: Array<String>) {
     // Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    val radius = 7.0
+    val pi = 3.14
     // Calculation
     var area: Double = pi * radius * radius
     // Output Concatenation
-    println("Radius: " + radius + ", Area: " + area)
+    println("Radius: $radius, Area: $area")
     // Logic check
     checkSize(area)
 }

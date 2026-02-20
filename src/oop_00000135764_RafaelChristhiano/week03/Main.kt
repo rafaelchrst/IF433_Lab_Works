e491f00d309de0e87e9faba6e221d7e486f97bb2
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-20 19:56:41 +0700

    week03: perbaikan struktur

--- src/oop_00000135764_RafaelChristhiano/week03/Main.kt
+++ src/oop_00000135764_RafaelChristhiano/week03/Main.kt
@@ -1,9 +1,40 @@
-println("\n=== TEST PLAYER ===")
+package oop_00000135764_RafaelChristhiano.week03
 
-val player = Player("Rafael")
+fun main() {
 
-player.addXp(50)
-println("Level sekarang: ${player.level}")
+    println("=== TEST EMPLOYEE ===")
 
-player.addXp(60)
-println("Level sekarang: ${player.level}")
+    val emp = Employee("Rafael", 5_000_000.0)
+    println("Nama: ${emp.name}")
+    println("Salary: ${emp.salary}")
+
+    emp.salary = -1000.0
+    println("Salary setelah update: ${emp.salary}")
+
+
+
+    println("\n=== TEST WEAPON ===")
+
+    val sword = Weapon("Excalibur", 300)
+
+    println("Damage awal: ${sword.damage}")
+    println("Tier awal: ${sword.tier}")
+
+    sword.damage = -50
+    sword.damage = 9999
+
+    println("Damage setelah update: ${sword.damage}")
+    println("Tier sekarang: ${sword.tier}")
+
+
+
+    println("\n=== TEST PLAYER ===")
+
+    val player = Player("Rafael")
+
+    player.addXp(50)
+    println("Level sekarang: ${player.level}")
+
+    player.addXp(60)
+    println("Level sekarang: ${player.level}")
+}

8364f869e95745de57dcfdb2796520ad12b77efe
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-20 19:10:41 +0700

    week02: implement selection logic for constructors

--- src/oop_00000135764_RafaelChristhiano/week02/Main.kt
+++ src/oop_00000135764_RafaelChristhiano/week02/Main.kt
@@ -5,19 +5,29 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
 
+    println("Pilih Jalur Pendaftaran:")
+    println("1. Dengan Jurusan")
+    println("2. Tanpa Jurusan")
+
+    print("Pilihan: ")
+    val choice = scanner.nextLine().toInt()
+
     print("Masukkan NIM: ")
     val nim = scanner.nextLine()
 
     print("Masukkan Nama: ")
     val name = scanner.nextLine()
 
-    print("Masukkan Jurusan: ")
-    val major = scanner.nextLine()
+    if (choice == 1) {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
 
-    if (nim.length == 10 && name.isNotBlank() && major.isNotBlank()) {
         val student = Student(nim, name, major)
-        println("Data mahasiswa berhasil dibuat!")
+        println("Mahasiswa dibuat dengan jurusan.")
+    } else if (choice == 2) {
+        val student = Student(nim, name)
+        println("Mahasiswa dibuat tanpa jurusan.")
     } else {
-        println("Input tidak valid. Mahasiswa tidak dibuat.")
+        println("Pilihan tidak valid.")
     }
 }

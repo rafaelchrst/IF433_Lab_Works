7dc72f5a3d06db69b06565c89ee7ff5bc3a4b186
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-03-13 10:41:19 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000135764_RafaelChristhiano/week06/Main.kt
+++ src/oop_00000135764_RafaelChristhiano/week06/Main.kt
@@ -1,9 +1,15 @@
+import oop_00000135764_RafaelChristhiano.week06.SmartCCTV
+import oop_00000135764_RafaelChristhiano.week06.SmartHomeHub
+import oop_00000135764_RafaelChristhiano.week06.SmartLamp
+import oop_00000135764_RafaelChristhiano.week06.SmartSpeaker
+
 fun processCheckout(method: PaymentMethod, amount: Double) {
     println("-> Memulai checkout...")
     method.pay(amount)
 }
 
 fun main() {
+
     val myWatch = Smartwatch()
     myWatch.showTime()
 
@@ -16,4 +22,21 @@ fun main() {
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 1500000.0)
+
+
+    println("\n=== SMART HOME SYSTEM ===")
+
+    val lamp = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
+
+
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }

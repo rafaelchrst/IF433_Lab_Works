eed35b5762e5cbb5238d61566490bcac53444d67
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-03-28 00:49:37 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000135764_RafaelChristhiano/week07/Main.kt
+++ src/oop_00000135764_RafaelChristhiano/week07/Main.kt
@@ -9,29 +9,18 @@ fun main() {
     val client = NetworkClient.createClient()
     client.connect()
 
-    println("\n=== TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
-
     println("\n=== TEST DATA CLASS ===")
     val data1 = DataUser("Alice", 22)
-    val data2 = DataUser("Alice", 22)
     println(data1)
-    println("Sama? ${data1 == data2}")
-
-    val data3 = data1.copy(age = 23)
-    println("Hasil Copy: $data3")
-
-    val (userName, userAge) = data1
-    println("Destructured: $userName berumur $userAge")
+    println("Sama? ${data1 == DataUser("Alice", 22)}")
 
     println("\n=== TEST SEALED CLASS ===")
-    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+    val response: ApiResponse = Success("Data berhasil ditarik!")
 
     val uiMessage = when (response) {
-        is ApiResponse.Success -> "Tampilkan: ${response.data}"
-        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        is Success -> "Tampilkan: ${response.data}"
+        is Error -> "Munculkan alert: ${response.message}"
+        is Loading -> "Tampilkan Spinner"
     }
+    println(uiMessage)
 }

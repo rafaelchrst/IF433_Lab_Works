02542d5b7d8ccaa513d52aca2f434cbdb9d150db
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-04-19 10:45:25 +0700

    week08: implement java interop using !!

--- src/oop_00000135764_RafaelChristhiano/week08/Main.kt
+++ src/oop_00000135764_RafaelChristhiano/week08/Main.kt
@@ -48,4 +48,9 @@ fun main() {
         println(e.message)
     }
 
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
+
 }

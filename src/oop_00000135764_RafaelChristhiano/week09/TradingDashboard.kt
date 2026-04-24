f08c431a7228af1670bb2efe806f03835d7e9df8
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-04-24 11:32:02 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000135764_RafaelChristhiano/week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_00000135764_RafaelChristhiano.week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 8.3, "CLOSED"),
+        TradeLog("XRPUSDT", "LONG", 5, 0.0, "OPEN"),
+        TradeLog("BTCUSDT", "LONG", 30, 20.0, "CLOSED")
+    )
+
+}

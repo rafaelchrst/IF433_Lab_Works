6af77ceb076b37ae34a29336a50fbbd41b58ad4e
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-05-07 23:55:47 +0700

    week10: implement generic function with return type

--- src/oop_00000135764_RafaelChristhiano/week10/GenericMethods.kt
+++ src/oop_00000135764_RafaelChristhiano/week10/GenericMethods.kt
@@ -2,4 +2,8 @@ package oop_00000135764_RafaelChristhiano.week10
 
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+
+    fun <T> processData(input: T): T {
+        return input // [cite: 60]
+    }
 }

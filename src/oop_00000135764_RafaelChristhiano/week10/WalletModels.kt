c8f1645991060ba9a7ac4b4243863ebfa378dd51
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-05-08 00:11:59 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000135764_RafaelChristhiano/week10/WalletModels.kt
@@ -0,0 +1,5 @@
+package oop_00000135764_RafaelChristhiano.week10 // Sesuaikan NIM & Nama
+
+data class Coin(val name: String, val balance: Double)
+
+data class Transaction(val id: String, val amount: Double)

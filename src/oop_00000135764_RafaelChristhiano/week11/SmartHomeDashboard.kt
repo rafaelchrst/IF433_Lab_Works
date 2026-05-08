6fd90e34b063f02439bc8709db175420c7cf4a25
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-05-08 10:43:26 +0700

    week11: (task) test full smart home configuration pipeline

--- src/oop_00000135764_RafaelChristhiano/week11/SmartHomeDashboard.kt
+++ src/oop_00000135764_RafaelChristhiano/week11/SmartHomeDashboard.kt
@@ -35,4 +35,9 @@ fun main() {
 
     val totalPower = homeDevices.run { sumOf { it.powerLoad } }
     println("Total Daya: $totalPower Watt")
+
+    homeDevices.forEach {
+        println(it.diagnose())
+    }
+
 }

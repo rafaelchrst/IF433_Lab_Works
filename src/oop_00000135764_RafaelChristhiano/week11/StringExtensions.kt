efe788291dde1934fa70caae1513aaa6f65aefda
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-05-08 10:15:19 +0700

    week11: create string extension functions

--- src/oop_00000135764_RafaelChristhiano/week11/StringExtensions.kt
+++ src/oop_00000135764_RafaelChristhiano/week11/StringExtensions.kt
@@ -1,9 +1,9 @@
 package oop_00000135764_RafaelChristhiano.week11
 
-fun string.addGreeting(): String {
+fun String.addGreeting(): String {
     return "Hello, $this"
 }
 
-fun string.repeatTimes(n: Int): String {
+fun String.repeatTimes(n: Int): String {
     return this.repeat(n)
 }

90f455b642d311e2ae2928de7fbd4a66b83a58c5
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-05-29 10:45:39 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_00000135764_RafaelChristhiano/week12/SmartFeeder.kt
+++ src/oop_00000135764_RafaelChristhiano/week12/SmartFeeder.kt
@@ -47,5 +47,6 @@ fun main() {
             println("Peringatan ke Pemilik: ${error.message}")
             println("(Opsional: Berikan chicken jerky secara manual)")
         }
+    println("\n=== PIPELINE SELESAI DIEKSEKUSI ===")
 }
 

9031fee95791731b67d373ada8715d841b9e5d33
rafaelchrst <rafael.sipahutar154@gmail.com>
2026-05-29 14:01:12 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000135764_RafaelChristhiano/week14/EcommerceTask.kt
+++ src/oop_00000135764_RafaelChristhiano/week14/EcommerceTask.kt
@@ -18,3 +18,45 @@ class BadOrderProcessor {
         println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
     }
 }
+
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+class CsvOrderRepository : OrderRepository {
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        java.io.File("orders.csv").printWriter().use { writer ->
+            writer.println("$itemName, $finalPrice, $customerType")
+        }
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(itemName: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(itemName: String) {
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        // Logika penghitungan diskon sementara (Akan kita refactor di Checkpoint 20)
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90
+            else -> basePrice
+        }
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+
+        // SRP & DIP Terpenuhi: Mengandalkan abstraksi interface, bukan class konkret/hardcoded
+        repo.saveOrder(itemName, finalPrice, customerType)
+        notifier.sendNotification(itemName)
+    }
+}
